599ddb2ac2d4909c894baf37ec01ba54cb43c3ab
Ray Samalo <raymond.samalo@gmail.com>
2025-11-27 20:55:07 +0800

    add working local

--- test_local_llm.py
+++ test_local_llm.py
@@ -12,53 +12,121 @@ from app.dependencies import (CONFIG, customer_account_status_service,
                               policy_service)
 
 model = CONFIG.model.version
-#response = ollama.generate(model=model, prompt='Why is the sky blue?')
-#print(response['response'])
+# response = ollama.generate(model=model, prompt='Why is the sky blue?')
+# print(response['response'])
 
 @tool
-def get_customer_id_by_name(name:str)->Optional[str]:
-    """ Get the customer ID for a customer name
-
-    Args:
-        name (str): customer name
-
-    Returns:
+def check_loan_recommendation(residency:str) -> bool:
     """
-    customer=customer_info_service.find_by_name(name)
+    given residency status determine whether to recommend loan or not
+    """
+    if residency is None:
+        return False
+    residency=residency.strip().lower() 
+    return residency in ["citizen","permanent-resident"]
+
+@tool
+def get_customer_interest_rate_percentage(risk: str) -> Optional[float]:
+    """ Get customer interest rate for risk 
+    """
+    risk = risk.strip().capitalize()
+    value = policy_service.get_interest_rate(risk)
+    if value is None:
+        return 0
+    return value
+
+
+@tool
+def get_customer_risk(credit_score: int, account_status: str) -> Optional[str]:
+    """ Get customer risk for credit score and account status
+    """
+    return policy_service.get_risk(credit_score, account_status)
+
+
+@tool
+def get_customer_residency_status(ID: int) -> Optional[str]:
+    """ Get customer residency status for a customer ID
+    """
+    return customer_residency_status_service.find_by_id(ID)
+
+
+@tool
+def get_customer_account_status(ID: int) -> Optional[str]:
+    """ Get customer account status for a customer ID
+    """
+    status = customer_account_status_service.find_by_id(ID)
+    if status is None:
+        return None
+    return str(status)
+
+
+@tool
+def get_customer_credit_score(ID: int) -> Optional[int]:
+    """ Get customer credit score for a customer ID
+    """
+    return customer_credit_score_service.find_by_id(ID)
+
+
+@tool
+def get_customer_info_by_id(ID: int) -> dict:
+    """
+    Get customer credit score, account status, residency 
+    """
+    residency = customer_residency_status_service.find_by_id(ID)
+    status = customer_account_status_service.find_by_id(ID)
+    credit_score = customer_credit_score_service.find_by_id(ID)
+    status = str(status)
+    residency = str(residency)
+    return {
+        "credit_score": credit_score,
+        "status": status,
+        "residency": str(residency)
+    }
+
+
+@tool
+def get_customer_id_by_name(name: str) -> Optional[str]:
+    """ Get the customer ID for a customer name
+    """
+    customer = customer_info_service.find_by_name(name)
     if customer is None:
         return None
     return str(customer.ID)
 
+
 @tool
-def get_customer_name_by_id(ID:int)->Optional[str]:
+def get_customer_name_by_id(ID: int) -> Optional[str]:
     """ Get the customer name for a customer id
     Args:
         ID (int): customer id
 
     Returns:
     """
-    customer=customer_info_service.find_by_id(ID)
+    customer = customer_info_service.find_by_id(ID)
     if customer is None:
         return None
     return str(customer.name)
 
-@tool
-def add(a: int, b: int) -> int:
-    """Adds two numbers together""" # this docstring gets used as the description
-    return a + b # the actions our tool performs
+
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "you're a helpful assistant"), 
-    ("human", "{input}"), 
+    ("system", "you're a helpful assistant"),
+    ("human", "{input}"),
     ("placeholder", "{agent_scratchpad}"),
 ])
 if __name__ == "__main__":
-    tools=[add, get_customer_id_by_name, get_customer_name_by_id]
+    tools = [
+        check_loan_recommendation,
+        get_customer_info_by_id,
+        get_customer_id_by_name,
+        get_customer_risk,
+        get_customer_interest_rate_percentage,
+        get_customer_name_by_id]
     # Initialize your Ollama model
-    llm = ChatGroq(model=CONFIG.model.groq_model)
+    llm = ChatGroq(model=CONFIG.model.groq_model, temperature=0.2)
     # llm = ChatOllama(model=model)
     agent = create_tool_calling_agent(llm, tools, prompt)
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=False)
-    response=agent_executor.invoke({"input": "who has customer id 2222", })
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    response = agent_executor.invoke(
+        {"input": "provide information on Kit to make loan recommendation including credit score, account status, risk, interest rate."})
     print(response)
     # Example interaction where the model doesn't need the tool
-    
